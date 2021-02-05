cd025614170d4931898326833dfec2d3796e42f0
Olha-Starykova <olenkalog@gmail.com>
2021-02-05 22:38:13 +0200

    com

--- js/promisification2.js
+++ js/promisification2.js
@@ -22,7 +22,7 @@
 
 // /*
 //  * Сейчас работает так
-//  */
+//  */ 
 // toggleUserState(users, 'Mango', logger);
 // toggleUserState(users, 'Lux', logger);
 
@@ -31,3 +31,27 @@
 //  */
 // toggleUserState(users, 'Mango').then(logger);
 // toggleUserState(users, 'Lux').then(logger); */
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+    return new Promise((resolve) => {
+    const updatedUsers = allUsers.map((user) =>
+        user.name === userName ? { ...user, active: !user.active } : user);
+    
+    resolve(updatedUsers);
+});
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+
+ toggleUserState(users, 'Mango').then(logger);
+ toggleUserState(users, 'Lux').then(logger); 
+
+
