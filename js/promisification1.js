969c1899f3f7c36277a3aaecdcb8c27f475ecf5f
Olha-Starykova <olenkalog@gmail.com>
2021-02-05 21:06:26 +0200

    com

--- js/promisification1.js
+++ js/promisification1.js
@@ -11,3 +11,4 @@
 // delay(2000).then(logger); // Resolved after 2000ms
 // delay(1000).then(logger); // Resolved after 1000ms
 // delay(1500).then(logger); // Resolved after 1500ms
+console.log('hi')
